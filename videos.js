53148ddc2a561409ecc28c71096ab92eea5504b1
omartins-zs <gabrielmatheus209@gmail.com>
2023-04-19 15:15:44 -0300

    Add File de Exportação dos Videos

---
+++ videos.js
@@ -0,0 +1,30 @@
+export default [
+  {
+    src: "./video/01.mp4",
+    title: "Editando fotos",
+  },
+  {
+    src: "./video/02.mp4",
+    title: "Live Stream",
+  },
+  {
+    src: "./video/03.mp4",
+    title: "Digitando",
+  },
+  {
+    src: "./video/04.mp4",
+    title: "Home office",
+  },
+  {
+    src: "./video/05.mp4",
+    title: "Likes",
+  },
+  {
+    src: "./video/06.mp4",
+    title: "Gota de água",
+  },
+  {
+    src: "./video/07.mp4",
+    title: "Código",
+  },
+];
